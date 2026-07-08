bdacb5bd954ae7021cd7a23cf07b30a87371d02d
MAGSelPad <150753146+MAGSelPad@users.noreply.github.com>
2026-07-08 19:11:55 +0000

    Añadir componente SelectorUI para seleccionar ciudades y mostrar información del clima

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Grid } from '@mui/material';
 import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
+import SelectorUI from './components/SelectorUI';
 
 function App() {
   
@@ -17,7 +18,7 @@ function App() {
         <Grid size={12} container sx={{ justifyContent: "right", alignItems: "center" }}>Elemento: Alertas <AlertUI description="No se preveen lluvias"/></Grid>
 
         {/* Selector */}
-        <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
+        <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector <SelectorUI/></Grid>
 
         {/* Indicadores */}
         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>  
